cf55b5d894e4245a43ca938adc0249a3647acdaa
lovin123 <95518107+lovin123@users.noreply.github.com>
2022-10-07 21:17:32 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -53,7 +53,7 @@ function Header({placeholder}) {
             {/* left */}
             <div onClick={() => router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image
-                    src="https://links.papareact.com/qd3"
+                    src="https://thumb.housinganywhere.com/img/logo-1200x630.png"
                     layout="fill"
                     objectFit="contain"
                     objectPosition="left"
